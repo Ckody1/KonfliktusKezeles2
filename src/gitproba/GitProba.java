717a6edcd989eda2c2631b0c78fff7a278208d3c
Szabo Gergo <szabo.gergo@akkszalezi.hu>
2022-03-11 15:26:58 +0100

    2.név megadva

--- src/gitproba/GitProba.java
+++ src/gitproba/GitProba.java
@@ -1,11 +1,8 @@
 package gitproba;
 public class GitProba {
     public static void main(String[] args) {
-<<<<<<< HEAD
         String nev1 = "SzB";
-=======
-        String nev1, nev2, nev3;
->>>>>>> e1afbca602737be5a0ddb9fba93af3c3c66b90d0
+        String nev2 = "SzG";
     }
     
 }
